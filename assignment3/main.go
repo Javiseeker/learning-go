d1e43373664befef7e0f62a3ebb964ad2cfa6a85
Javier Felix Alonso Lopez <javieralonso123@outlook.es>
2026-01-12 18:56:51 +0000

    added task assignment 3 changes
    Signed-off-by: Javier Alonso <jalonsol@churchofjesuschrist.org>

--- assignment3/main.go
+++ assignment3/main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <fileName>")
+		os.Exit(1)
+	}
+
+	fileName := os.Args[1]
+	fmt.Println("File name:", fileName)
+
+	// Open the file
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			fmt.Println("Error closing file:", err)
+		}
+	}()
+
+	// Create a buffer to store the file contents
+	var contents bytes.Buffer
+
+	// Copy file contents to the buffer
+	_, err = io.Copy(&contents, file)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+
+	// Print the file contents
+	fmt.Println("\nFile contents:")
+	fmt.Println(contents.String())
+}
